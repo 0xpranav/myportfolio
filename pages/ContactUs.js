db9f3e05a486ec665567ec94ff5d15ef6888c428
Pranav Garg <gargprnv@gmail.com>
2022-11-06 23:44:50 +0530

    with build, change api key

--- pages/ContactUs.js
+++ pages/ContactUs.js
@@ -28,7 +28,7 @@ export const ContactUs = () => {
 
     emailjs
       .sendForm(
-        process.env.REACT_APP_SERVICE_ID,
+       process.env.REACT_APP_SERVICE_ID,
         process.env.REACT_APP_TEMPLATE_ID,
         form.current,
         process.env.REACT_APP_PUBLIC_KEY,
